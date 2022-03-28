9ca397bebf30326088d6d23e1a2967c31aed9d83
fleisch <markus.fleischauer@uni-jena.de>
2022-03-28 12:11:42 +0200

    add time stamp to package

--- .github/scripts/mac-create-identity.zsh
+++ .github/scripts/mac-create-identity.zsh
@@ -1,5 +1,5 @@
-./sirius_dist/build/tmp/artifacts/sirius-${{ steps.sirius_version.outputs.value }}-macOS-x86-64.pkg
-./sirius_dist/build/tmp/artifacts/sirius-${{ steps.sirius_version.outputs.value }}-macOS-x86-64-headless.pkg
+#./sirius_dist/build/tmp/artifacts/sirius-${{ steps.sirius_version.outputs.value }}-macOS-x86-64.pkg
+#./sirius_dist/build/tmp/artifacts/sirius-${{ steps.sirius_version.outputs.value }}-macOS-x86-64-headless.pkg
 #rm -rf ./*.dmg
 #jpackage --name Unattach --app-version $VERSION --description "$DESCRIPTION" --vendor "$VENDOR" --copyright "$COPYRIGHT" \
 #    --license-file LICENSE \
@@ -10,7 +10,7 @@
 #/usr/bin/productsign --timestamp --sign '"'"$MACOS_IDENTITY_ID"'"' ./sirius_dist/build/tmp/artifacts/sirius-$VERSION-osx64.pkg  ./sirius_dist/build/tmp/artifacts/sirius-$VERSION-macOS-x86-64.pkg
 /usr/bin/productsign --timestamp --sign '"'"$MACOS_IDENTITY_ID"'"' ./sirius_dist/build/tmp/artifacts/sirius-$VERSION-osx64-headless.pkg ./sirius_dist/build/tmp/artifacts/sirius-$VERSION-macOS-x86-64-headless.pkg
 # Upload pkg for verification.
-REQUEST_UUID=$(xcrun altool --notarize-app --primary-bundle-id "app.$APP_NAME-$VERSION" -u "$" -p "$MACOS_IDENTITY_ID" --file./sirius_dist/build/tmp/artifacts/sirius-$VERSION-macOS-x86-64-headless.pkg | grep RequestUUID | awk '{print $3}')
+REQUEST_UUID=$(xcrun altool --notarize-app --primary-bundle-id "app.$APP_NAME-$VERSION" -u "$MACOS_IDENTITY_ID" -p "$MACOS_IDENTITY_ID" --file ./sirius_dist/build/tmp/artifacts/sirius-$VERSION-macOS-x86-64-headless.pkg | grep RequestUUID | awk '{print $3}')
 # Wait for verification to complete.
 while xcrun altool --notarization-info "$REQUEST_UUID" -u "$MACOS_APPLE_ID" -p "$MACOS_IDENTITY_ID" | grep "Status: in progress" > /dev/null; do
   echo "Verification in progress..."
